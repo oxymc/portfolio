b681021fd73d1c9ae4322e2e934a480834ea7b2b
Maxym Balagur <maxymbalagur@MacBook-Pro-Maxym.local>
2022-05-01 20:42:23 +0300

    changed styles

--- src/components/Way.jsx
+++ src/components/Way.jsx
@@ -1,14 +1,24 @@
 
 
 const Way = ({translate, isTranslated}) => {
-    const title = "My way"
+    const title = "Learning now"
     const text = !isTranslated ?
         "У самурая немає цілі, є тільки шлях..." :
         'The samurai has no goal, there is only a way...'
+    const learning = "Redux"
+
     return (
         <section id="way">
             <div className="container row center">
-                <div className="col s12"><h2>{title}</h2></div>
+                <div className="col s12">
+                    <h2>{title}</h2>
+                    <font>
+                        {learning}
+                        <div className="progress">
+                            <div className="indeterminate"></div>
+                        </div>
+                    </font>
+                </div>
                 <div className="col s12">
                     <i className="large material-icons">all_inclusive</i>
                     <font>{text}</font>
